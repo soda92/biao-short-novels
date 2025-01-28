8d6d76c73e42b99c2d65120d4ea5df0c8f980bd4
sodacris <wjxa20152015@gmail.com>
2025-01-28 17:08:10 +0800

    json test

--- scrape.py
+++ scrape.py
@@ -4,9 +4,26 @@ headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
 }
 
+args = {
+    "action": "getalbum",
+    "__biz": "",
+    "album_id": "3329839945947119618",
+    "count": "10",
+    "begin_msgid": "2247484370",
+    "begin_itemidx": "1",
+    "uin": "",
+    "key": "",
+    "pass_ticket": "",
+    "wxtoken": "",
+    "devicetype": "",
+    "clientversion": "",
+    "appmsg_token": "",
+    "x5": "0",
+    "f": "json",
+}
+
 r = requests.get(
-    url="https://mp.weixin.qq.com/mp/appmsgalbum?&action=getalbum&album_id=3329839945947119618",
-    headers=headers,
+    url="https://mp.weixin.qq.com/mp/appmsgalbum", headers=headers, params=args
 )
 
-print(r.status_code, r.content)
+print(r.status_code, r.json())
