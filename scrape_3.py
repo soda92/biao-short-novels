3d0598c695b524d48ae99b84d15612203ef30f20
sodacris <wjxa20152015@gmail.com>
2025-01-28 21:30:41 +0800

    merge parse logic for scrape_3

--- scrape_3.py
+++ scrape_3.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import requests
 
+from bs4 import BeautifulSoup
+import re
+
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
 }
@@ -13,3 +16,42 @@ r = requests.get(url=url, headers=headers)
 print(r.status_code)
 
 Path("s_2.html").write_bytes(r.content)
+
+
+html = Path("s_2.html").read_text(encoding="utf8")
+soup = BeautifulSoup(html, "html.parser")
+texts = soup.findAll(string=True)
+
+
+def visible(element):
+    if element.parent.name in ["style", "script", "[document]", "head", "title"]:
+        return False
+    elif re.match("<!--.*-->", element):
+        return False
+    return True
+
+
+visible_texts = list(filter(visible, texts))
+
+visible_texts = visible_texts[: visible_texts.index("预览时标签不可点")]
+t = "".join(visible_texts)
+
+while "\n\n\n" in t:
+    t = t.replace("\n\n\n", "\n\n")
+v = list(
+    map(
+        lambda x: x.strip().replace("\n", "\n\n"),
+        filter(lambda x: x != "", t.split("\n\n")),
+    )
+)
+
+title = v[0].replace(" | ", "-")
+content = "\n\n".join(title[3:])
+
+p = f"""+++
+date = 'date'
+title = '{v[0]}'
++++
+
+{content}
+"""
