0f8afda8c1df1c6f368894947b56d85c191caca7
sodacris <wjxa20152015@gmail.com>
2025-01-28 22:40:41 +0800

    surround with __name__ check to prevent side effect on import from other module

--- scrape_1.py
+++ scrape_1.py
@@ -39,66 +39,69 @@ def get():
     write_path(dest, r.content.decode())
 
 
-get()
+if __name__ == "__main__":
+    get()
 
-content = Path("generated_html/start.html").read_text(encoding="utf8")
+    content = Path("generated_html/start.html").read_text(encoding="utf8")
 
-pattern = '<span class="js_article_create_time album__item-info-item">([0-9]*)</span>'
-t = re.findall(pattern, content)[0]
+    pattern = (
+        '<span class="js_article_create_time album__item-info-item">([0-9]*)</span>'
+    )
+    t = re.findall(pattern, content)[0]
 
-pattern = "https://mmbiz.qpic.cn/mmbiz_jpg/[0-9a-zA-Z]+/300"
-img = re.findall(pattern, content)[0]
+    pattern = "https://mmbiz.qpic.cn/mmbiz_jpg/[0-9a-zA-Z]+/300"
+    img = re.findall(pattern, content)[0]
 
-pattern = 'data-link="(.*)"'
-url = re.findall(pattern, content)[0]
-url = url.replace("&amp;", "&")
+    pattern = 'data-link="(.*)"'
+    url = re.findall(pattern, content)[0]
+    url = url.replace("&amp;", "&")
 
-pattern = 'data-msgid="([0-9]+)"'
-msgid = re.findall(pattern, content)[0]
+    pattern = 'data-msgid="([0-9]+)"'
+    msgid = re.findall(pattern, content)[0]
 
-pattern = 'data-title="(.*)"'
-title = re.findall(pattern, content)[0]
+    pattern = 'data-title="(.*)"'
+    title = re.findall(pattern, content)[0]
 
-structure = (
-    """
-{
-  "base_resp": {
-    "exportkey_token": "",
-    "ret": 0
-  },
-  "getalbum_resp": {
-    "article_list": [
-      {
-        "cover_img_1_1": "{img}",
-        "cover_theme_color": {
-          "b": "30",
-          "g": "35",
-          "r": "31"
-        },
-        "create_time": "{t}",
-        "is_pay_subscribe": "0",
-        "is_read": "0",
-        "item_show_type": "0",
-        "itemidx": "1",
-        "key": "3885769611_2247484415_1",
-        "msgid": "{msgid}",
-        "pos_num": "76",
-        "title": "{title}",
-        "tts_is_ban": "0",
-        "url": "{url}",
-        "user_read_status": "0"
-      }
-    ]
-  }
-}""".replace("{t}", t)
-    .replace("{img}", img)
-    .replace("{url}", url)
-    .replace("{msgid}", msgid)
-    .replace("{title}", title)
-)
+    structure = (
+        """
+    {
+    "base_resp": {
+        "exportkey_token": "",
+        "ret": 0
+    },
+    "getalbum_resp": {
+        "article_list": [
+        {
+            "cover_img_1_1": "{img}",
+            "cover_theme_color": {
+            "b": "30",
+            "g": "35",
+            "r": "31"
+            },
+            "create_time": "{t}",
+            "is_pay_subscribe": "0",
+            "is_read": "0",
+            "item_show_type": "0",
+            "itemidx": "1",
+            "key": "3885769611_2247484415_1",
+            "msgid": "{msgid}",
+            "pos_num": "76",
+            "title": "{title}",
+            "tts_is_ban": "0",
+            "url": "{url}",
+            "user_read_status": "0"
+        }
+        ]
+    }
+    }""".replace("{t}", t)
+        .replace("{img}", img)
+        .replace("{url}", url)
+        .replace("{msgid}", msgid)
+        .replace("{title}", title)
+    )
 
-print(structure)
+    print(structure)
 
-dst = Path("generated_json").joinpath(f"{msgid}.json")
-clean_file_dir(dst)
-write_path(dst, structure)
+    dst = Path("generated_json").joinpath(f"{msgid}.json")
+    clean_file_dir(dst)
+    write_path(dst, structure)
