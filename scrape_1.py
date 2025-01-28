3e0f14e706eee1dffcaa05556184d3f710ff9529
sodacris <wjxa20152015@gmail.com>
2025-01-28 21:08:15 +0800

    merge parse with fetch

--- scrape_1.py
+++ scrape_1.py
@@ -1,17 +1,86 @@
-import requests
 from pathlib import Path
+import re
+import shutil
+import requests
 
-headers = {
-    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
-}
 
-args0 = {
-    "action": "getalbum",
-    "album_id": "3329839945947119618",
-}
+def get():
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
+    }
 
-r = requests.get(
-    url="https://mp.weixin.qq.com/mp/appmsgalbum", headers=headers, params=args0
+    args0 = {
+        "action": "getalbum",
+        "album_id": "3329839945947119618",
+    }
+
+    r = requests.get(
+        url="https://mp.weixin.qq.com/mp/appmsgalbum", headers=headers, params=args0
+    )
+
+    Path("start.html").write_bytes(r.content)
+
+
+get()
+
+content = Path("start.html").read_text(encoding="utf8")
+
+pattern = '<span class="js_article_create_time album__item-info-item">([0-9]*)</span>'
+t = re.findall(pattern, content)[0]
+
+pattern = "https://mmbiz.qpic.cn/mmbiz_jpg/[0-9a-zA-Z]+/300"
+img = re.findall(pattern, content)[0]
+
+pattern = 'data-link="(.*)"'
+url = re.findall(pattern, content)[0]
+url = url.replace("&amp;", "&")
+
+pattern = 'data-msgid="([0-9]+)"'
+msgid = re.findall(pattern, content)[0]
+
+pattern = 'data-title="(.*)"'
+title = re.findall(pattern, content)[0]
+
+structure = (
+    """
+{
+  "base_resp": {
+    "exportkey_token": "",
+    "ret": 0
+  },
+  "getalbum_resp": {
+    "article_list": [
+      {
+        "cover_img_1_1": "{img}",
+        "cover_theme_color": {
+          "b": "30",
+          "g": "35",
+          "r": "31"
+        },
+        "create_time": "{t}",
+        "is_pay_subscribe": "0",
+        "is_read": "0",
+        "item_show_type": "0",
+        "itemidx": "1",
+        "key": "3885769611_2247484415_1",
+        "msgid": "{msgid}",
+        "pos_num": "76",
+        "title": "{title}",
+        "tts_is_ban": "0",
+        "url": "{url}",
+        "user_read_status": "0"
+      }
+    ]
+  }
+}""".replace("{t}", t)
+    .replace("{img}", img)
+    .replace("{url}", url)
+    .replace("{msgid}", msgid)
+    .replace("{title}", title)
 )
 
-Path("start.html").write_bytes(r.content)
+print(structure)
+
+shutil.rmtree("json")
+Path("json").mkdir(parents=True, exist_ok=True)
+Path("json").joinpath(msgid + ".json").write_text(structure, encoding="utf8")
--- scrape_2.py
+++ scrape_2.py
@@ -1,10 +1,27 @@
 from pathlib import Path
 import requests
 import json
+import time
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 14_4_1) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4.1 Safari/605.1.15"
 }
+"""
+empty/error response:
+{
+  "base_resp": {
+    "exportkey_token": "",
+    "ret": 0
+  },
+  "getalbum_resp": {
+    "base_info": {
+      "is_first_screen": "0"
+    },
+    "continue_flag": "0",
+    "reverse_continue_flag": "1"
+  }
+}
+"""
 
 
 def get_last_msgid(obj):
@@ -38,19 +55,43 @@ def get(msgid):
     assert r.status_code == 200
     r2 = json.loads(r.content.decode())
 
-    msgid = get_last_msgid(r2)
+    return r2
 
-    Path(f"json/{msgid}.json").write_bytes(r.content)
+
+def write(obj):
+    msgid = get_last_msgid(obj)
+    print(msgid)
+
+    Path(f"json/{msgid}.json").write_text(
+        json.dumps(obj, indent=2, ensure_ascii=False), encoding="utf8"
+    )
 
 
 if __name__ == "__main__":
     import glob
 
     f = list(sorted(glob.glob("json/*.json"), reverse=True))
+    assert len(f) == 1
 
     f = f[0]
     r = json.loads(Path(f).read_text(encoding="utf8"))
 
     msgid = get_last_msgid(r)
     print(msgid)
-    get(msgid)
+
+    obj = get(msgid)
+
+    while "article_list" in obj["getalbum_resp"]:
+        write(obj)
+        time.sleep(10)
+
+        f = list(sorted(glob.glob("json/*.json")))
+        latest = Path(f[0]).read_text(encoding="utf8")
+
+        r = json.loads(latest)
+
+        msgid = get_last_msgid(r)
+
+        obj = get(msgid)
+
+    print("finished")
